2e92d7d61025cee3dae8420837177d7119b0824d
Lilia Paz <lpla1154@gmail.com>
2019-11-19 11:57:09 -0800

    adding navbar and new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import ExternalApi from "./components/ExternalApi";
-import NavBar from './components/NavBar';
-import PrivateRoute from './components/PrivateRoute';
-import Profile from './components/Profile';
+// import ExternalApi from "./components/ExternalApi";
+import NavBar from './components/NavBar'
+import PrivateRoute from './components/PrivateRoute'
+import Profile from './components/Profile'
+import Search from './components/Search'
+import Home from './components/Home'
+// import List from './components/List';
 
 function App () {
   return (
@@ -15,8 +19,11 @@ function App () {
           <NavBar />
         </header>
         <Switch>
-          <Route path='/' exact />
-          <PrivateRoute path='/profile' component={Profile} />
+          <Route path='/' exact component={Home} />
+          <PrivateRoute exact path='/profile' component={Profile} />
+          <PrivateRoute exact path='/search' component={Search} />
+          {/* <Route exact path='/search' component={Search} /> */}
+          {/* <Route path='/list' component={List} /> */}
         </Switch>
       </BrowserRouter>
     </div>
