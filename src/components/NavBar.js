b357d625e72caee35076627ee33d73d080b0b42f
Lilia Paz <lpla1154@gmail.com>
2019-11-19 11:55:17 -0800

    adding navbar options so it's constant on every page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,65 @@
-import React from "react";
-import { useAuth0 } from "../react-auth0-spa";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { useAuth0 } from '../react-auth0-spa'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button
+} from 'reactstrap'
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+  const [isOpen, setOpen] = useState(false)
+
+  const toggle = () => {
+    setOpen(!isOpen)
+  }
 
   return (
     <div>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
-      )}
-
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-
-      {isAuthenticated && (
-        <span>
-          <Link to="/">Home</Link>&nbsp;
-          <Link to="/profile">Profile</Link>
-          <Link to="/external-api">External API</Link>
-        </span>
-      )}
+      <Navbar color={'dark'} dark expand={'md'} className={'mb-5'}>
+        <NavbarBrand href='/'>Something</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='ml-auto' navbar>
+            {!isAuthenticated && (
+              <NavItem >
+                <Button type={'button'} onClick={() => loginWithRedirect({ redirect_uri: 'http://localhost:3000/search' })}>Login</Button>
+              </NavItem>
+            )}
+            {isAuthenticated && (
+              <NavItem >
+                <Button type={'button'} onClick={() => logout()}>
+                Logout
+                </Button>
+              </NavItem>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  );
-};
+  )
 
-export default NavBar;
+  // return (
+  //   <div>
+  //     {!isAuthenticated && (
+  //       <button onClick={() => loginWithRedirect({})}>Log in</button>
+  //     )}
+
+  //     {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+  //     {isAuthenticated && (
+  //       <span>
+  //         <Link to="/">Home</Link>&nbsp;
+  //         <Link to="/profile">Profile</Link>
+  //         <Link to="/external-api">External API</Link>
+  //       </span>
+  //     )}
+  //   </div>
+  // )
+}
+
+export default NavBar
